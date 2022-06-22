c970daecac19a40ab3dc493390b4355bba289701
David Morrow <quickbasic44@outlook.com>
2022-06-22 15:09:56 -0400

    fixed Works Perfectly

--- WPFValueConverters/ColorSwitcher.cs
+++ WPFValueConverters/ColorSwitcher.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Globalization;
 using System.Linq;
 using System.Text;
@@ -14,13 +15,22 @@ namespace WPFValueConverters
         {
             string red = "C32616";
             string green = "3EC316";
+            string blue = string.Format(" {0:x6}", Color.Blue.ToArgb());
+            blue = blue.Remove(0, 3);
+            string purple = string.Format(" {0:x6}", Color.Purple.ToArgb());
+            purple = purple.Remove(0, 3);
+
 
             switch (value.ToString())
             {
                 case "1":
                     return "#" + parameter + red;
                 case "2":
-                    return "*" + parameter + green;
+                    return "#" + parameter + green;
+                case "3":
+                    return "#" + parameter + blue;
+                case "4":
+                    return "#" + parameter + purple;
                 default:
                     return null;
             }
